47edd1deae3077af3afebb22ad361d4e0e24fa8f
kingreturneds <129239251+kingreturneds@users.noreply.github.com>
2023-08-20 16:37:08 +0800

    node

---
+++ node.js
@@ -0,0 +1,19 @@
+console.log("hellow world");
+console.log("this is my new stuff");
+console.log("XD");
+
+let hehehe  =["t","h","i","s"];
+console.log(hehehe);
+
+for(let i in hehehe){
+    console.log(hehehe[i]);
+
+}
+let XD = ["one for all","all for one"];
+for(let i in XD){
+    console.log(XD[i]);
+}
+
+for (let i =0; i < 10; i++){  // i = index
+    console.log("nyawa" + i);
+}
